a01a4a4fa9f338faf99e65c7c22184bbb2420080
adeelwebdev0 <adeelwebdev0@gmail.com>
2026-06-19 12:25:33 +0500

    complete frontend

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", notesRoutes);
+app.use("/api/notes", notesRoutes);
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is runnig odn this port ${PORT} ✅✅`);
